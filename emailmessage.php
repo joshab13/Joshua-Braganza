65eb9b3548101a892d58816bdfe4fb8311196221
joshab13 <59944943+joshab13@users.noreply.github.com>
2020-01-26 01:31:53 -0500

    Emailing functionality and positioning
    Fixed the php code and fixed footer positioning for tablet and laptop view

---
+++ emailmessage.php
@@ -0,0 +1,25 @@
+<?php
+
+if (isset($_POST{'submit'])) {
+
+
+$name = $_POST['contactName'];
+$visitor_email = $_POST['contactEmail'];
+$message = $_POST['contactMessage'];
+$email_subject = $_POST['contactSubject'];
+
+$to = "jb14go@brocku.ca";
+$headers = "From: ".$visitor_email;
+
+
+
+
+
+
+mail($to, $email_subject, $message, $headers);
+
+header("Location: index.php?mailsend");
+
+}
+
+?>
